d21dfaa79b4e98d96aa4f7ad002f98b49f385002
Yulan Zhang <100871214+Zhangyl09@users.noreply.github.com>
2023-09-02 15:01:07 -0400

    Update plot_x_p.m

--- examples/example2/MATLAB/plot_x_p.m
+++ examples/example2/MATLAB/plot_x_p.m
@@ -3,7 +3,7 @@
  % 2022/08/10
  % 
  % This script plots the subtangent lines obtained from C++ implementation of adjoint subgradients
- % evalaution system.
+ % evaluation system.
  %
  % Example 4 for CACE paper
  %
